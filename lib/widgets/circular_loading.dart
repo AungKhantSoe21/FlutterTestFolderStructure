c821d03af8bdb0e044d83ea8072290bb72c6a633
Zaw Min Tun <zawmintun.tp@gmail.com>
2021-09-17 17:10:06 +0630

    Update circular_loading.dart

--- lib/widgets/circular_loading.dart
+++ lib/widgets/circular_loading.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 
 class CircularLoading extends StatelessWidget {
-  const CircularLoading({ Key? key }) : super(key: key);
+  const CircularLoading({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Center(
-          child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircularProgressIndicator(backgroundColor: Colors.black, color: Colors.white,strokeWidth: 2),
-              SizedBox(height:15),
-              Text('Loading ...', style: TextStyle(color: Colors.black),)
-            ],
-          )),
-        );
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircularProgressIndicator(color: Colors.black, strokeWidth: 2),
+          SizedBox(height: 15),
+          Text(
+            'Loading ...',
+            style: TextStyle(color: Colors.black),
+          )
+        ],
+      ),
+    );
   }
 }
